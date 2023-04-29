4081c8a49f0cdbb8905bac3a12c7b5055228fee5
Ajay Ganesh <ajganesh2002@gmail.com>
2023-04-29 17:55:37 -0400

    added readme fetching

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,16 @@ class GHProjectParse {
   async listProjects() {
     const repos = await this.octokit.paginate("GET /user/repos");
     const filtered = repos.filter((elt) => elt.topics?.includes("project"));
-    return filtered;
+    console.log(filtered)
+    const readme_promises = filtered.map((elt) => {
+      return this.octokit.rest.repos.getReadme({
+        owner: elt.owner.login ?? "",
+        repo: elt.name,
+      });
+    });
+    const readmes = await Promise.all(readme_promises);
+    console.log(readmes);
+    return readmes;
   }
 }
 
