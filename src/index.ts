3748e140aa80547575f8ddbaafbeb899ad950c21
Ajay Ganesh <ajganesh2002@gmail.com>
2023-04-29 00:44:05 -0400

    config modifications

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import { Octokit } from "octokit";
+
+class GHProjectParse {
+  octokit;
+
+  constructor(auth: string) {
+    this.octokit = new Octokit({ auth: auth });
+  }
+
+  async listProjects() {
+    const repos = await this.octokit.paginate("GET /user/repos");
+    const filtered = repos.filter((elt) => elt.topics?.includes("portfolio"));
+    return filtered;
+  }
+}
+
+export default GHProjectParse;
