cbb63209567e891c086f468c7baa888817a4a046
Ajay Ganesh <ajganesh2002@gmail.com>
2023-05-01 05:46:06 -0400

    decoded readmes

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,29 @@ class GHProjectParse {
     this.octokit = new Octokit({ auth: auth });
   }
 
-  async listProjects() {
+  async parseProjects() {
     const repos = await this.octokit.paginate("GET /user/repos");
-    const filtered = repos.filter((elt) => elt.topics?.includes("project"));
-    console.log(filtered)
-    const readme_promises = filtered.map((elt) => {
+    const project_repos = repos.filter((elt) => elt.topics?.includes("project"));
+    const readme_promises = project_repos.map((elt) => {
       return this.octokit.rest.repos.getReadme({
         owner: elt.owner.login ?? "",
         repo: elt.name,
       });
     });
     const readmes = await Promise.all(readme_promises);
-    console.log(readmes);
-    return readmes;
+    const descriptions = readmes.map((readme) => {
+      return atob(readme.data.content);
+    });
+
+    const full_projects = project_repos.map((proj, idx) => { 
+      return {
+        "name": proj.name,
+        "mdDesc": descriptions[idx],
+        "gh_link": proj.html_url
+      }
+    })
+
+    return full_projects;
   }
 }
 
